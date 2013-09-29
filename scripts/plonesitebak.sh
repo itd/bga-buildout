505ca6bf58799e0ee24b730386946cdd57d6940d
bga <bga@tool.net>
2013-09-29 23:14:23 +0000

    Tweak the backup script.

--- scripts/plonesitebak.sh
+++ scripts/plonesitebak.sh
@@ -2,16 +2,18 @@
 PATHS=/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin
 export PATH=PATHS:$PATH
 
-PROJ=atotool
-TARGET=kbendl@stc-17111s.nrel.gov
+PROJ=bga
+TARGET=$PROJ.tool.net
+PEM=$PROJ.pem
 
 ### Tarball and copy the production files to a local backup repo
 ### for quick recovery.
 TD=`date +%Y%m%d%H%m`
-cd /opt/sites/$PROJ/$PROJ-buildout/var
+cd /home/ohtogo/sites/bga-buildout/var
 ../bin/fullbackup
 sleep 5
 tar -czf var-fullbackup.tgz backups blobstoragebackups
 sleep 30
 echo -e "Copying var-fullbackup.tgz to var-fullbackup-$TD.tgz"
-scp var-fullbackup.tgz $TARGET:/web/$PROJ/data/$PROJ-var-fullbackup-$TD.tgz
+scp  var-fullbackup.tgz $PROJ@$TARGET:/web/$PROJ/backups/$PROJ-var-fullbackup-$TD.tgz
+
